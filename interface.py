cf268ecb48371fe3920533ebb868c6cba53434ec
Hugo Puhlmann <hugopuhlmann@gmail.com>
2011-06-29 22:54:56 -0300

    Adding basic CLI skel.

--- interface.py
+++ interface.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python2
 
+import sys
+from optparse import OptionParser
+
 def main():
-    pass
+    interactive = sys.stdin.isatty() and sys.stdout.isatty()
+
+    parser = OptionParser(usage="usage: %prog [options]")
+    parser.add_option('-m', '--machine', dest="machine_file",
+                      help="File to read expression from, instead of stdin.")
+    parser.add_option('-b', '--batch',
+                      action="store_false", dest="interactive",
+                      help="Run in batch mode: Evaluates the expression an retuns
+                        normal form.")
+    parser.add_option('-i', '--interactive',
+                      action="store_true",  dest="interactive", default=interactive,
+                      help="Run in interactive mode: Reads an input and evaluate
+                        expression step-by-step.")
+
+    options, args = parser.parse_args()
+
+if __name__ == '__main__':
+    main()
