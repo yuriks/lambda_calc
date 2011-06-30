8a7179206d1d00d4ab127e53675cbd15de7eca7b
yuriks <yuriks@yuriks.net>
2011-06-29 23:08:44 -0300

    Added stubs to parse.py

--- parse.py
+++ parse.py
@@ -2,3 +2,11 @@
 # data Var = 'a'..'z'
 # data Apply = ( '(' Expr Expr ')' ) | ( Expr Expr )
 # data LExpr = '$' Var ( '.' Expr )?
+
+# String -> AST
+def parse(string):
+	pass
+
+# AST -> String
+def synthetize(ast):
+	return ""
