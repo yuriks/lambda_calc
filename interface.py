b954118359cb41286be69c21229b99c7c7f5458c
yuriks <yuriks@yuriks.net>
2011-06-29 22:54:11 -0300

    Slightly fixed AST grammar.

--- interface.py
+++ interface.py
@@ -2,7 +2,7 @@
 
 # data Expr = Var | Apply | LExpr
 # data Var = 'a'..'z'
-# data Apply = '('? Expr Expr ')'?
+# data Apply = ( '(' Expr Expr ')' ) | ( Expr Expr )
 # data LExpr = '$' Var '.' Expr
 
 def main():
