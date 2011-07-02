0b90bb161530f51a8f6ca46a76fd162da68d62d8
brunodea <brunodea@inf.ufsm.br>
2011-07-01 23:17:15 -0300

    comeco da implementacao da funcao de step.

--- operations.py
+++ operations.py
@@ -1,15 +1,28 @@
+from parse import *
+
 #retorna uma lista com as variaveis livres do ast.
 def freeVars(ast):
     if not isRedex(ast):
         return ast
     pass
 
-
 #faz a aplicacao de um redex (reduz ele).
-#retorna uma tupla: (normal,num_steps).
-def apply(redex):
+#retorna uma lista com cada arvore de cada passo.
+def apply(ast):
     pass
 
+def step(ast):
+    if ast[0] == S_VAR:
+        return set([ast[0]])
+    elif ast[0] == S_LAMBDA:
+        return set(step(ast[2])).difference(set(ast[1]))
+    elif ast[0] == S_APPLY:
+        pass
+
 #verifica se uma AST eh um redex.
-def isRedex(ast):
+def isRedex(expression):
+    pass
+
+#retorna true se 'ast' nao tem variaveis livres.
+def isClosed(ast):
     pass
