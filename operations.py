1181d7f44bc6708bebf8286ef07d0a70bd46c141
brunodea <brunodea@inf.ufsm.br>
2011-07-03 14:53:41 -0300

    arrumei a funcao freeVars.

--- operations.py
+++ operations.py
@@ -3,12 +3,16 @@ from parse import *
 #retorna uma lista com as variaveis livres do ast.
 def freeVars(ast):
     if ast[0] == S_VAR:
-        return set([ast[0]])
+        return set([ast[1]])
     elif ast[0] == S_LAMBDA:
-        return set(step(ast[2])).difference(set(ast[1]))
+        return set(freeVars(ast[2])).difference(set([ast[1]]))
     elif ast[0] == S_APPLY:
-        if not isRedex(ast):
-            return set([ast])
+        return set(freeVars(ast[1])).union(set(freeVars(ast[2])))
+
+#faz a substituicao de lam_term por new_exp em term.
+def replace(lam_term, new_exp, term):
+    #todo fazer recursivamente.
+    pass
 
 #faz a aplicacao de um redex (reduz ele).
 def reduct(redex):
@@ -21,11 +25,21 @@ def reduct(redex):
     lam_term = exp1[1]
     return exp1[2].replace(lam_term, exp2)
 
+def betaReduction(ast):
+    pass
+         
+
 def step(ast):
-    if exp[0] == S_APPLY:
-        if isRedex(ast):
-            ast = reduct(ast)
-            return ast           
+    if ast[0] == S_APPLY and isRedex(ast):
+        ast = reduct(ast)
+    elif ast[0] == S_LAMBDA:
+        s = step(ast[2])
+        res = (ast[0],ast[1],s)
+        ast = s
+    elif ast[0] == S_VAR:
+        pass
+    return ast
+
 
 #verifica se uma expressao eh um redex.
 def isRedex(exp):
