02248e57112b50a4c434246248e746cdbde4ab5f
brunodea <brunodea@inf.ufsm.br>
2011-07-04 00:25:29 -0300

    comentarios adicionados.

--- operations.py
+++ operations.py
@@ -11,9 +11,8 @@ FOUR = (1,SUCC,THREE)
 FIVE = (1,SUCC,FOUR)
 SIX = (1,SUCC,FIVE)
 
-
-#retorna uma lista com as variaveis livres do ast.
 def freeVars(ast):
+    """Retorna uma lista com as variaveis livres de AST."""
     if ast[0] == S_VAR:
         return set([ast[1]])
     elif ast[0] == S_LAMBDA:
@@ -21,9 +20,11 @@ def freeVars(ast):
     elif ast[0] == S_APPLY:
         return set(freeVars(ast[1])).union(set(freeVars(ast[2])))
 
-#lam_term eh o nome do termo lambda. Ex.: $x.t (x eh o termo lambda).
-#faz a substituicao de lam_term por new_exp em term.
 def replace(lam_term, new_exp, term):
+    """
+        Faz a substituicao de lam_term por new_exp em term.
+        lam_term é o nome do termo lambda. Ex.: $x.t (x é o termo lambda).  
+    """
     if term[0] == S_VAR:
         if term[1] == lam_term:
             return new_exp
@@ -37,13 +38,18 @@ def replace(lam_term, new_exp, term):
         rhs = replace(lam_term,new_exp,term[2])
         return (term[0],lhs,rhs)
 
-#faz a aplicacao de um redex (reduz ele).
 def betaApply(lhs,rhs):
+    """ 
+        Faz uma aplicação de rhs em lhs.
+    """
     if lhs[0] == S_LAMBDA:
         return replace(lhs[1], rhs, lhs[2])
     return (S_APPLY, lhs, rhs)
 
 def betaReduction(ast):
+    """
+        Faz a beta redução de AST até o seu estado normal.
+    """
     result = ast
     last = None
     while(result != last):
@@ -52,6 +58,9 @@ def betaReduction(ast):
     return result
 
 def step(ast):
+    """
+        Faz uma etapa da beta redução de AST.
+    """
     if ast[0] == S_VAR:
         return ast
     elif ast[0] == S_LAMBDA:
@@ -70,9 +79,11 @@ def step(ast):
 
         return result
 
-
-#verifica se uma expressao eh um redex.
 def isRedex(exp):
+    """ 
+        Verifica se uma expressão é um redex.
+        Dúvida: Quando uma expressão é um redex?
+    """
     empty = set()
     return (freeVars(exp) != empty) #isso nao ta certo.
 
