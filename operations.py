9ad8af2ccca81c75cb4b7b88c49adc0d6f749445
brunodea <brunodea@inf.ufsm.br>
2011-07-06 01:44:45 -0300

    comecei a implementacao da funcao alphaReduction. not working.

--- operations.py
+++ operations.py
@@ -4,7 +4,7 @@ SUCC = (0,'n',(0,'f',(0,'x',(1,(2,'f'),(1,(1,(2,'n'),(2,'f')),(2,'x'))))))
 ADD = (0,'m',(0,'w',(0,'q',(0,'r',(1,(1,(2,'m'),(2,'q')),(1,(1,(2,'w'),(2,'q')),(2,'r')))))))
 ID = (0,'k',(2,'k'))
 
-ZERO = (0,'g',(0,'y',(2,'y')))
+ZERO = (0,'f',(0,'x',(2,'x')))
 ONE = (1,SUCC,ZERO)
 TWO = (1,SUCC,ONE)
 THREE = (1,SUCC,TWO)
@@ -26,6 +26,9 @@ def replace(lam_term, new_exp, term):
         Faz a substituicao de lam_term por new_exp em term.
         lam_term eh o nome do termo lambda. Ex.: $x.t (x eh o termo lambda).  
     """
+#    if not substIsSafe(term,lam_term,new_exp):
+#        term = alphaReduction(term,lam_term,new_exp)
+
     if term[0] == S_VAR:
         if term[1] == lam_term:
             return new_exp
@@ -54,6 +57,48 @@ def betaReduction(ast):
     while(isRedex(ast)):
         ast = step(ast)
     return ast
+
+def substIsSafe(exp1, var, exp2):
+    """
+        Verifica se a substituicao de var por exp2 em exp1 eh segura.
+    """
+    fvars2 = freeVars(exp2)
+    nexp = replace(var,exp2,exp1)
+
+    fvarsn = freeVars(nexp)
+
+    isValid = (set(fvarsn).intersection(set(fvars2)) == set(fvars2))
+
+    return isValid
+
+def alphaReduction(exp1,var,exp2):
+    """
+        faz a reducao alpha da variavel var de exp1 relativo a exp2.
+    """
+    if exp1[0] == S_VAR:
+        if var == exp1[1]:
+            return exp2
+        else:
+            return exp1
+    elif exp1[0] == S_LAMBDA:
+        if exp1[1] == var:
+            return exp1
+        elif exp1[1] not in freeVars(exp2):
+            return (S_LAMBDA, exp1[1], alphaReduction(exp1[2],var,exp2))
+        else:
+            lamt = exp1[1] + '\''
+            fvars = freeVars((S_APPLY,exp1,exp2))
+            while(lamt == var or lamt in fvars):
+                lamt = lamt + '\''
+
+            nexp1 = replace(exp1[1],lamt,exp1[2])
+            return (S_LAMBDA, lamt, alphaReduction(nexp1[2],var,exp2))
+
+    elif exp1[0] == S_APPLY:
+        lhs = exp1[1]
+        rhs = exp1[2]
+        return (S_APPLY, alphaReduction(lhs,var,exp2), alphaReduction(rhs,var,exp2))
+
 
 def step(ast):
     """
