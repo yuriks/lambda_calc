f71bbd238cfd2e4231cb80daf73f1d301cca22f0
yuriks <yuriks@yuriks.net>
2011-07-05 22:14:19 -0300

    Renamed token type constants to have a T_ prefix.

--- parse.py
+++ parse.py
@@ -7,7 +7,7 @@
 import re
 
 token_re = re.compile(r'\s*(?:(\w+)|(\$)|(\.)|(\()|(\))|(.))')
-VAR = 1; LAMBDA = 2; DOT = 3; L_PAREN = 4; R_PAREN = 5; ERROR = 6; EOF = 7
+T_VAR = 1; T_LAMBDA = 2; T_DOT = 3; T_LPAREN = 4; T_RPAREN = 5; T_ERROR = 6; T_EOF = 7
 
 # $x.$y.x y x
 # (S_LAMBDA, 'x', (S_LAMBDA, 'y', (S_APPLY, (S_APPLY, (S_VAR, 'x'), (S_VAR, 'y') ), (S_VAR, 'x'))))
@@ -23,7 +23,7 @@ class TokenizationError(Exception):
 
 def tokenize(string):
     for m in token_re.finditer(string):
-        if m.lastindex == ERROR:
+        if m.lastindex == T_ERROR:
             raise TokenizationError("Unknown token `%s`" % (m.group(m.lastindex)))
         else:
             yield (m.lastindex, m.group(m.lastindex))
@@ -34,30 +34,30 @@ class ParseError(Exception):
 
 def parseVar(tokens):
     t, v = tokens.pop(0)
-    if t != VAR:
+    if t != T_VAR:
         raise ParseError("Expected Var")
     return (S_VAR, v)
 
 def parseApplyParen(tokens):
     t, v = tokens.pop(0)
-    if t != L_PAREN:
+    if t != T_LPAREN:
         raise ParseError("Expected `(`")
     expr = parseApply(tokens)
     t, v = tokens.pop(0)
-    if t != R_PAREN:
+    if t != T_RPAREN:
         raise ParseError("Expected `)`")
     return expr
 
 def parseLExpr(tokens):
     t, v = tokens.pop(0)
-    if t != LAMBDA:
+    if t != T_LAMBDA:
         raise ParseError("Expected `$`")
     t, var = tokens.pop(0)
-    if t != VAR:
+    if t != T_VAR:
         raise ParseError("Expected Var")
-    if tokens[0][0] == DOT:
+    if tokens[0][0] == T_DOT:
         t, v = tokens.pop(0)
-        if t != DOT:
+        if t != T_DOT:
             raise ParseError("Expected `.`")
         body = parseApply(tokens)
     else:
@@ -66,11 +66,11 @@ def parseLExpr(tokens):
 
 def parseExpr(tokens):
     peek = tokens[0][0]
-    if peek == LAMBDA:
+    if peek == T_LAMBDA:
         return parseLExpr(tokens)
-    elif peek == L_PAREN:
+    elif peek == T_LPAREN:
         return parseApplyParen(tokens)
-    elif peek == VAR:
+    elif peek == T_VAR:
         return parseVar(tokens)
     else:
         raise ParseError("Expected LExpr, Apply or Var")
@@ -85,7 +85,7 @@ def parseApply(tokens):
     expr_list = []
 
     peek = tokens[0][0]
-    while peek != R_PAREN and peek != EOF:
+    while peek != T_RPAREN and peek != T_EOF:
         expr_list.append(parseExpr(tokens))
         peek = tokens[0][0]
 
@@ -94,9 +94,9 @@ def parseApply(tokens):
 # String -> AST
 def parse(string):
     token_list = list(tokenize(string))
-    token_list.append((EOF, ''))
+    token_list.append((T_EOF, ''))
     ast = parseApply(token_list)
-    if token_list[0][0] != EOF:
+    if token_list[0][0] != T_EOF:
         raise ParseError("Unexpected trailing input: %s" % (token_list[0][1],))
     return ast
 
